2fa24ff586b7eea0476d2dd9179673b21195bec7
Nico Schlömer <nico.schloemer@gmail.com>
2018-07-19 11:30:44 +0200

    add speed test

---
+++ test/test_speed.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#
+from matplotlib import path
+import numpy
+import polypy
+import perfplot
+
+
+def test_speed(n=3):
+    poly_pts = [[0, 0], [0, 1], [1, 1], [1, 0]]
+    poly0 = path.Path(poly_pts)
+    poly1 = polypy.Polygon(poly_pts)
+
+    def _mpl_poly(pts):
+        return poly0.contains_points(pts)
+
+    def _polypy_is_inside(pts):
+        return poly1.is_inside(pts)
+
+    numpy.random.seed(0)
+
+    perfplot.show(
+        setup=lambda n: numpy.random.rand(n, 2),
+        kernels=[_mpl_poly, _polypy_is_inside],
+        n_range=[2**k for k in range(n)],
+        labels=["matplotlib.path.contains_points", "polypy.is_inside"],
+        logx=True,
+        logy=True,
+        xlabel="num points",
+    )
+    return
+
+
+def benchmark():
+    poly_pts = [[0, 0], [0, 1], [1, 1], [1, 0]]
+    poly1 = polypy.Polygon(poly_pts)
+    pts = numpy.random.rand(5000000, 2)
+    poly1.is_inside(pts)
+    return
+
+
+if __name__ == "__main__":
+    # test_speed(20)
+    benchmark()
